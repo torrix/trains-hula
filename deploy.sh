161f81dca4d9f22c37aa469f6b99963756d4114b
Matt Fletcher <matt@torrix.uk>
2024-04-28 17:22:01 +0100

    Change GHA runner

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,8 @@ set -e
 
 echo "Deploying application ..."
 
-# Update codebase
 git pull origin main
 
-# Install dependencies based on lock file
 php8.3 /usr/local/bin/composer install --no-interaction --prefer-dist --optimize-autoloader
-
-# Build frontend
-npm run build
 
 echo "🚀 Application deployed!"
